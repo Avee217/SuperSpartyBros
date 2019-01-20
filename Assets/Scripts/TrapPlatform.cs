0640e90bb6ffdac0d76d9fe902f2e8ead8f3ecbc
Aveenash <avee217@gmail.com>
2019-01-21 01:52:45 +0530

    Upload Game

---
+++ Assets/Scripts/TrapPlatform.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TrapPlatform : MonoBehaviour {
+
+	Animator _animator;
+	public bool trap;
+
+	void Awake () 
+	{
+		_animator = GetComponent<Animator> ();
+		if (_animator == null) // if Animator is missing
+			Debug.LogError ("Animator component missing from this gameobject");
+		trap = false;
+
+	}
+
+		
+	void OnTriggerEnter2D(Collider2D other)
+	{
+		if (other.gameObject.tag == "Player")
+		{
+			trap = true;
+			_animator.SetBool("TrapTrigger",trap);
+		}
+
+	}
+
+
+}
